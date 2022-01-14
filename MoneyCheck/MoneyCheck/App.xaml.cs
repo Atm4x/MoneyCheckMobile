dcddf4f2a3193e2b0e9fb621421d2e8f7e5813a3
ArtHack <mrtaktplay@gmail.com>
2022-01-14 21:41:00 +0300

    Добавьте файлы проекта.

---
+++ MoneyCheck/MoneyCheck/App.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MoneyCheck
+{
+    public partial class App : Application
+    {
+        public static List<object> Transactions = new List<object>();
+        public static List<object> Debtors = new List<object>();
+
+       
+
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ MoneyCheck/MoneyCheck/Pages/MasterPage.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MoneyCheck.Pages
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MasterPage : MasterDetailPage
+    {
+        public Label NotFoundLabel;
+        public MasterPage()
+        {
+            InitializeComponent();
+            IsPresented = false;
+            Refresh();
+        }
+
+
+        public void Refresh()
+        {
+            if (App.Debtors.Count <= 0)
+            {
+                MyDebtors.Children.Clear();
+                NotFoundLabel = new Label()
+                {
+                    TextColor = Color.FromHex("#B122F4"),
+                    FontSize = 20,
+                    HorizontalOptions = LayoutOptions.Center,
+                    Text = "Здесь ничего нет"
+                };
+
+                MyDebtors.Children.Add(NotFoundLabel);
+            }
+            if(App.Transactions.Count <= 0)
+            {
+                MyTransactions.Children.Clear();
+                NotFoundLabel = new Label()
+                {
+                    TextColor = Color.FromHex("#B122F4"),
+                    FontSize = 20,
+                    HorizontalOptions = LayoutOptions.Center,
+                    Text = "Здесь ничего нет"
+
+                };
+
+                MyTransactions.Children.Add(NotFoundLabel);
+            }
+        }
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            IsPresented = true;
+        }
+    }
+}
